c356dcedef4ab888a5019d5a80c2e352fa9857ae
rizrmdhn <rizrmdhn.work@gmail.com>
2023-04-08 10:18:35 +0800

    Pertemuan Ke 1

---
+++ Pertemuan Ke 1/array2.php
@@ -0,0 +1,6 @@
+<?php
+$names = ["id" => 001, "name" => "Pitrasacha Adtia", "age" => "30", "address" => ["city" => "Samarinda", "country" => "Indonesia"]];
+var_dump($names);
+
+var_dump($names["name"]);
+var_dump($names["address"]["country"]);
